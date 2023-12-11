3fac8fc18f4e9364376518ed78f9a45f92787469
revirakaputra <revirakaputra@gmail.com>
2023-12-11 14:48:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, useLocation, Navigate } from "react-router-dom";
+import React, { Suspense } from "react";
+import {
+  NotFound,
+  Loader,
+  ScrollToTop,
+  Navbar,
+  Footer,
+  BlogDetail,
+  BlogIndex,
+} from "./components";
+import Blog from "./pages/Blog";
+import Kontak from "./pages/Kontak";
+import Tentang from "./pages/Tentang";
+const Home = React.lazy(() => import("./pages/Home"));
+const Paket = React.lazy(() => import("./pages/Paket"));
 
 function App() {
+  const location = useLocation();
+  const isFalse = location.pathname.includes("404");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ScrollToTop />
+      {isFalse || <Navbar />}
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tentang" element={<Tentang />} />
+          <Route path="/paket" element={<Paket />} />
+          <Route path="/kontak" element={<Kontak />} />
+          <Route path="/blog" element={<Blog />}>
+            <Route index element={<BlogIndex />} />
+            <Route path=":blog_path" element={<BlogDetail />} />
+          </Route>
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate replace to="/404" />}></Route>
+        </Routes>
+      </Suspense>
+      {isFalse || <Footer />}
+    </>
   );
 }
 
